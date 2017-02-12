3bac6ad8986a98945219fc217ec49731c01b657c
Hayden McAfee <hayden@outlook.com>
2017-02-11 16:11:30 -0800

    Some work on the client-side to try to get a server connection working

--- Tribes2/t2stats.cs
+++ Tribes2/t2stats.cs
@@ -13,11 +13,101 @@
 // #include = support/kill_callbacks.cs
 // ---------------------------------------------------------------------------
 
-function processKillCallback(%type, %killer, %victim, %weapon, %i_die, %i_win, %suicide, %tk) {
+function t2stats_processKillCallback(%type, %killer, %victim, %weapon, %i_die, %i_win, %suicide, %tk) {
     echo("Kill detected (" @ %type @ "): '" @ %killer @ "' killed '" @ %victim @ "', weapon: '" @ %weapon @ "'.");
     echo("VICTIM: " @ %victim.guid @ "/" @ %victim.name);
     echo("KILLER: " @ %killer.guid @ "/" @ %killer.name);
 }
 
-Callback.add("KillCallback", processKillCallback);
+function StatsConnection::onDNSFailed(%this)
+{
+    error("StatsConnection DNS lookup failed.");
+}
+
+function StatsConnection::onConnectFailed(%this)
+{
+    error("StatsConnection connection failed.");
+}
+
+function StatsConnection::onDNSResolved(%this)
+{
+    echo("StatsConnection DNS resolved.");
+}
+
+function StatsConnection::onConnected(%this)
+{
+    echo("StatsConnection connected.");
+    //%this.send(%this.data);
+}
+
+function StatsConnection::onDisconnect(%this)
+{
+    echo("StatsConnection disconnected.");
+    %this.delete();
+}
+
+function StatsConnection::onLine(%this, %line)
+{
+    echo( "StatsConnection line received: " @ %line );
+}
+
+function t2stats_reportKill() {
+    %weaponName = "Spinfusor";
+    %victimGuid = 12345;
+    %victimName = "Player 1";
+    %killerGuid = 54321;
+    %killerName = "Player 2";
+    %reporterGuid = 11111;
+    %reporterName = "Player 3";
+    %matchTime = "00:02:03.23";
+    %matchStartTime = "2014-05-06T22:24:55Z";
+    %matchDuration = "00:12:02.00";
+    %matchMapName = "Katabatic";
+    %matchServerIpAddress = "192.168.0.12";
+    %matchServerPort = 28000;
+
+    %postBody =
+        "{" @
+        "    weaponName: \"" @ %weaponName @ "\"," @
+        "    victim: {" @
+        "        tribesGuid: " @ %victimGuid @ "," @
+        "        name: \"" @ %victimName @ "\"," @
+        "    }," @
+        "    killer: {" @
+        "        tribesGuid: " @ %killerGuid @ "," @
+        "        name: \"" @ %killerName @ "\"," @
+        "    }," @
+        "    reporter: {" @
+        "        tribesGuid: " @ %reporterGuid @ "," @
+        "        name: \"" @ reporterName @ "\"," @
+        "    }," @
+        "    matchTime: \"" @ %matchTime @ "\"," @
+        "    match: {" @
+        "        startTime: \"" @ %matchStartTime @ "\"," @
+        "        duration: \"" @ %matchDuration @ "\"," @
+        "        map: {" @
+        "            name: \"" @ %matchMapName @ "\"," @
+        "        }," @
+        "        server: {" @
+        "            ipAddress: \"" @ %matchServerIpAddress @ "\"," @
+        "            port: " @ %matchServerPort @ "," @
+        "        }" @
+        "    }" @
+        "}";
+
+	%data = 
+        "POST /Kills HTTP/1.1\r\n" @
+        "Host: localhost:5000\r\n" @
+        "User-Agent: Tribes2\r\n" @
+        "Content-Type: application/json\r\n" @
+        "Content-Length: " @ strlen(%postBody) @ "\r\n" @
+        "Connection: close\r\n" @
+        "\r\n" @ %postBody;
+    
+    %connection = new TCPObject(StatsConnection);
+    %connection.data = %data;
+    %connection.connect("localhost:5000");
+}
+
+Callback.add("KillCallback", t2stats_processKillCallback);
 echo("T2 Stats Activated.");
